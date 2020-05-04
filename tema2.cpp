815f91bd4515ca1c184dd6171c1868dd9baa0e08
Claudia Panait <claudia-oana.panait@my.fmi.unibuc.ro>
2020-05-05 00:27:56 +0300

    Fixes solved, new errors found
    There is an error with the main program that i don't know how to fix

--- tema2.cpp
+++ tema2.cpp
@@ -1,28 +1,29 @@
 #include<iostream>
 
-class Vector {
+class Vector{
 public:
     int *a, length;
+    char semn;
 public:
-    Vector(int len);
+    Vector(int len, char s);
 
     Vector(Vector const &a2, bool citire);
 
     ~Vector();
 
     void print(std::ostream &os) const {
-        os << "P=";
-        os << a[0];
-        for (int i = 1; i <= length - 1; i++)
-            os << " + " << a[i] << "X^" << i;
+        os << semn;
+        for (int i = 0; i <= length - 1; i++)
+            os << a[i];
     }
 
     friend std::ostream &operator<<(std::ostream &os, const Vector &V);
 };
 
-Vector::Vector(int len) {
+Vector::Vector(int len, char s) {
     int i;
     length = len;
+    semn = s;
     a = new int[len];
     for (i = 0; i < len; i++)
         std::cin >> a[i];
@@ -32,20 +33,13 @@ Vector::Vector(Vector const &a2, bool citire) {
     int i;
     if (citire) {
         length = a2.length + 1;
-        a = new int[length];
-        for (i = 1; i < a2.length; i++)
-            a[i] = a2.a[i];
-        a[0] = 0;
+        i = 1;
     }
-
-    if (!citire) {
-        length = a2.length + 2;
-        a = new int[length];
-        for (i = 2; i < a2.length; i++)
-            a[i] = a2.a[i];
-        a[0] = 0;
-        a[1] = 0;
-    }
+    semn = a2.semn;
+    a = new int[length];
+    a[0] = 0;
+    for (int k = i; k < a2.length; k++)
+        a[i] = a2.a[i];
 }
 
 Vector::~Vector() {
@@ -58,16 +52,49 @@ std::ostream &operator<<(std::ostream &os, const Vector &V) {
 }
 
 class Nr_Natural_Mare {
+protected:
+    Vector A;
 public:
-    static Vector A;
+    void verification(const Vector &V, const Vector &V2, int k, int i, int len) {
+        if (k == i) {
+            Vector A(A, true);
+            A.a[1] = A.a[1] % 10;
+            A.a[0] = A.a[1] / 10;
+        } else {
+            if (k > i) {
+                for (int m = k; m >= 0; m--) {
+                    if (V.a[k] < 10) {
+                        A.a[len] = V.a[k];
+                        len--;
+                    } else {
+                        A.a[len] = V.a[k] % 10;
+                        V.a[k - 1] = V.a[k] / 10 + V.a[k - 1];
+                        len--;
+                    }
+                }
+            }
+            if (k < i) {
+                for (int m = i; m >= 0; m--) {
+                    if (V2.a[k] < 10) {
+                        A.a[len] = V2.a[k];
+                        len--;
+                    } else {
+                        A.a[len] = V2.a[k] % 10;
+                        V2.a[k - 1] = V2.a[k] / 10 + V2.a[k - 1];
+                        len--;
+                    }
+                }
+            }
+        }
+    }
 
-    static Vector adunare(int nrcif, Vector V, int nrcif2, Vector V2) {
-        int sum, k = nrcif, i = nrcif2, len;
+    Vector adunare(int nrcif, const Vector& V, int nrcif2, const Vector& V2) {
+        int sum, k = nrcif - 1, i = nrcif2 - 1, len;
         if (k > i)
-            Vector A(k);
+            Vector A(k, '+');
         else
-            Vector A(i);
-        len = A.length;
+            Vector A(i, '+');
+        len = A.length - 1;
         while ((k != 0) && (i != 0)) {
             sum = V.a[k] + V2.a[i];
             if (sum < 10)
@@ -84,184 +111,66 @@ public:
             i--;
             len--;
         }
-        if (k == i == 0) {
-            Vector A(A, true);
-            A.a[1] = (V.a[1] + V2.a[0]) % 10;
-            A.a[0] = (V.a[1] + V2.a[0]) / 10;
-        } else {
-            if (k > i) {
-                while (k != 0) {
-                    if (V.a[k] < 10) {
-                        A.a[len] = V.a[k];
-                        k--;
-                        len--;
-                    } else {
-                        A.a[len] = V.a[k] % 10;
-                        V.a[k - 1] = V.a[k] / 10 + V.a[k - 1];
-                        k--;
-                        len--;
-                    }
-                }
-                if (V.a[0] > 10) {
-                    Vector A(A, true);
-                    A.a[1] = V.a[0] % 10;
-                    A.a[0] = V.a[0] / 10;
-                }
-            }
-            if (k < i) {
-                while (i != 0) {
-                    if (V2.a[i] < 10) {
-                        A.a[len] = V2.a[i];
-                        i--;
-                        len--;
-                    } else {
-                        A.a[len] = V2.a[i] % 10;
-                        V2.a[i - 1] = V2.a[2] / 10 + V2.a[i - 1];
-                        i--;
-                        len--;
-                    }
-                }
-                if (V2.a[0] > 10) {
-                    Vector A(A, true);
-                    A.a[1] = V2.a[0] % 10;
-                    A.a[0] = V2.a[0] / 10;
-                }
-            }
-        }
+        verification(V, V2, k, i, len);
         return A;
     }
 
-    static Vector inmultire(int nrcif, Vector V, int nrcif2, Vector V2) {
-        int mul, k = nrcif, i = nrcif2, len;
-        if (k > i)
-            Vector A(k);
-        else
-            Vector A(i);
-        len = A.length;
-        while ((k != 0) && (i != 0)) {
-            mul = V.a[k] * V2.a[i];
-            if (mul < 10)
-                A.a[len] = mul;
-            if (mul > 10) {
-                A.a[len] = mul % 10;
-                mul = mul / 10;
-                if (k > i)
-                    A.a[len - 1] = V.a[k - 1] + mul;
-                else
-                    A.a[len - 1] = V2.a[k - 1] + mul;
+    Vector inmultire(int nrcif, const Vector& V, int nrcif2, const Vector& V2) {
+        int mul, k = nrcif, i = nrcif2, len, m, n;
+        Vector A(k + i, '+');
+        for (m = k - 1; m >= 0; m--) {
+            len = A.length - (A.length - m + i);
+            for (n = i - 1; n >= 0; n--) {
+                mul = V.a[k] * V2.a[i];
+                A.a[len] = mul % 10 + A.a[len];
+                A.a[len - 1] = mul / 10 + A.a[len - 1];
+                len--;
             }
+        }
+        return A;
+
+    }
+};
+
+class Nr_Intreg_Mare : public Nr_Natural_Mare{
+private:
+    Vector B;
+public:
+    void calcul(int sum, Vector &V, int &k, int &i, int &len, int operatie) {
+        while (i != 0) {
+            if (A.a[i] > V.a[k]) {
+                V.a[k - 1]--;
+                V.a[k] = V.a[k] * 10;
+            }
+            sum = V.a[k] - A.a[i];
+            B.a[len] = sum;
             k--;
             i--;
             len--;
         }
-        if (k == i == 0) {
-            if (V.a[i] * V2.a[i] < 10)
-                A.a[0] = V.a[i] * V2.a[i];
-            if (V.a[i] * V2.a[i] > 10 && V.a[i] * V2.a[i] < 100) {
-                Vector A(A, true);
-                A.a[1] = (V.a[1] * V2.a[0]) % 10;
-                A.a[0] = (V.a[1] * V2.a[0]) / 10;
-            }
-            if (V.a[i] * V2.a[i] > 100) {
-                Vector A(A, false);
-                A.a[2] = (V.a[1] * V2.a[0]) % 10;
-                A.a[1] = (V.a[1] * V2.a[0]) / 10;
-                A.a[0] = (V.a[1] * V2.a[0]) / 100;
-            }
-
-        } else {
-            if (k > i) {
-                while (k != 0) {
-                    if (V.a[k] < 10) {
-                        A.a[len] = V.a[k];
-                        k--;
-                        len--;
-                    } else {
-                        A.a[len] = V.a[k] % 10;
-                        V.a[k - 1] = V.a[k] / 10 + V.a[k - 1];
-                        k--;
-                        len--;
-                    }
-                }
-                if (V.a[0] > 10) {
-                    Vector A(A, true);
-                    A.a[1] = V.a[0] % 10;
-                    A.a[0] = V.a[0] / 10;
-                }
-            }
-            if (k < i) {
-                while (i != 0) {
-                    if (V2.a[i] < 10) {
-                        A.a[len] = V2.a[i];
-                        i--;
-                        len--;
-                    } else {
-                        A.a[len] = V2.a[i] % 10;
-                        V2.a[i - 1] = V2.a[2] / 10 + V2.a[i - 1];
-                        i--;
-                        len--;
-                    }
-                }
-                if (V2.a[0] > 10) {
-                    Vector A(A, true);
-                    A.a[1] = V2.a[0] % 10;
-                    A.a[0] = V2.a[0] / 10;
-                }
-            }
-        }
     }
-};
 
-class Nr_Intreg_Mare : public Nr_Natural_Mare {
-public:
-    static Vector B;
-
-    static Vector adunarein(char semn, int nrcif, Vector V) {
+    Vector adunarein(int nrcif, Vector V) {
         int sum;
-        char semn1;
-        if (semn == '+') {
-            std::cout << semn;
+        if (((V.semn == '+') && (A.semn == '+')) || ((V.semn == '-') && (A.semn == '-')))
             return Nr_Natural_Mare::adunare(nrcif, V, A.length, A);
-        }
-        if (semn == '-') {
-            int sum, k = nrcif, i = A.length, len;
+        if (((V.semn == '+') && (A.semn == '-')) || ((V.semn == '-') && (A.semn == '+'))) {
+            int sum = 0, k = nrcif, i = A.length, len;
 
             if (k > i)
-                Vector B(k);
+                Vector B(k, V.semn);
             else
-                Vector B(i);
+                Vector B(i, A.semn);
             len = B.length;
             if (k > i) {
-                semn1 = '-';
-                while (i != 0) {
-                    if (A.a[i] > V.a[k]) {
-                        V.a[k - 1]--;
-                        V.a[k] = V.a[k] * 10;
-                    }
-                    sum = V.a[k] - A.a[i];
-                    B.a[len] = sum;
-                    k--;
-                    i--;
-                    len--;
-                }
+                calcul(sum, V, k, i, len, V.a[k] - A.a[i]);
                 for (int j = k; j >= 0; j--) {
                     B.a[len] = V.a[k];
                     len--;
                 }
             }
             if (k < i) {
-                semn1 = '+';
-                while (i != 0) {
-                    if (A.a[i] > V.a[k]) {
-                        V.a[k - 1]--;
-                        V.a[k] = V.a[k] * 10;
-                    }
-                    sum = A.a[k] - V.a[i];
-                    B.a[len] = sum;
-                    k--;
-                    i--;
-                    len--;
-                }
+                calcul(sum, V, k, i, len, A.a[k] - V.a[i]);
                 for (int j = k; j >= 0; j--) {
                     B.a[len] = V.a[k];
                     len--;
@@ -270,44 +179,21 @@ public:
 
             if (k == i) {
                 if (V.a[0] > A.a[0]) {
-                    semn1 = '-';
-                    while (i != 0) {
-                        if (A.a[i] > V.a[k]) {
-                            V.a[k - 1]--;
-                            V.a[k] = V.a[k] * 10;
-                        }
-                        sum = V.a[k] - A.a[i];
-                        B.a[len] = sum;
-                        k--;
-                        i--;
-                        len--;
-                    }
+                    B.semn = '-';
+                    calcul(sum, V, k, i, len, V.a[k] - A.a[i]);
                 }
 
                 if (V.a[0] < A.a[0]) {
-                    semn1 = '+';
-                    std::cout << semn1;
-                    while (i != 0) {
-                        if (A.a[i] > V.a[k]) {
-                            V.a[k - 1]--;
-                            V.a[k] = V.a[k] * 10;
-                        }
-                        sum = A.a[k] - V.a[i];
-                        B.a[len] = sum;
-                        k--;
-                        i--;
-                        len--;
-                    }
+                    B.semn = '+';
+                    calcul(sum, V, k, i, len, A.a[k] - V.a[i]);
                 }
             }
-            std::cout << semn1;
             return B;
         }
     }
 
-    static Vector inmultirein(char semn, int nrcif, Vector V) {
+    Vector inmultirein(int nrcif, const Vector& V) {
         B = Nr_Natural_Mare::inmultire(nrcif, V, A.length, A);
-        std::cout << semn;
         return B;
     }
 
@@ -316,18 +202,21 @@ public:
 int main() {
     int n, m, p;
     char semn;
+    Nr_Natural_Mare V1, V2;
+    Nr_Intreg_Mare V3;
     std::cin >> n >> m >> p;
-    Vector vect(n);
-    Vector vect2(m);
-    Vector vect3(p);
     std::cin >> semn;
-    Vector V1 = Nr_Natural_Mare::adunare(n, vect, m, vect2);
+    Vector vect(n, '+');
+    Vector vect2(m, '+');
+    Vector vect3(p, semn);
+
+    V1 = V1.adunare(n, vect, m, vect2);
     std::cout << "Adunare= " << V1 << std::endl;
-    Vector V2 = Nr_Natural_Mare::inmultire(n, vect, m, vect2);
+    V2= V2.inmultire(n, vect, m, vect2);
     std::cout << "Inmultire= " << V2 << std::endl;
-    Vector V3 = Nr_Intreg_Mare::adunarein(semn, p, vect3);
+    V3=V3.adunarein(p, vect3);
     std::cout << "Adunare= " << V3 << std::endl;
-    V3 = Nr_Intreg_Mare::inmultirein(semn, p, vect3);
+    V3=V3.inmultirein(p, vect3);
     std::cout << "Inmultire= " << V3;
 
     return 0;
